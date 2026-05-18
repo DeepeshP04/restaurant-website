444698aa3222e1552e70c4fc1d440bc95364594d
DeepeshP04 <pratapdeepesh4@gmail.com>
2026-05-18 17:34:38 +0530

    add home carousel section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import Navbar from "./components/Navbar";
+import Home from "./components/Home"
+import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App() {
   return (
     <>
     <Navbar></Navbar>
+    <div className="main-content">
+      <Home />
+      </div>
     </>
   )
 }
